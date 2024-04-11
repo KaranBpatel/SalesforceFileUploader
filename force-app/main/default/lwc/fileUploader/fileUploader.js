bfdb9b60d552fa68958939060aafcc1c8d9909c7
Karan Patel <pkarantanvi@gmail.com>
2024-04-11 17:16:10 +0530

    Completed Compoent FileUploader

--- force-app/main/default/lwc/fileUploader/fileUploader.js
+++ force-app/main/default/lwc/fileUploader/fileUploader.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class FileUploader extends LightningElement {}
+export default class FileUploader extends LightningElement {
+
+     myRecordId='0015h00001ezA2SAAU';
+
+    get acceptedFormats() {
+        return ['.pdf', '.png'];
+    }
+    handleUploadFinished(ev){
+        const file= ev.detail.files;
+        console.log('Files >7>',file.length);
+    }
+}
