215811db3b3a999abdca40240aadf9c57c4b2192
Karan Patel <pkarantanvi@gmail.com>
2024-04-12 13:25:27 +0530

    update

--- force-app/main/default/lwc/fileUploader/fileUploader.js
+++ force-app/main/default/lwc/fileUploader/fileUploader.js
@@ -1,25 +1,8 @@
-import { LightningElement,api, wire } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
-import getUploadedFiles from '@salesforce/apex/FileController.getFiles';
+import { LightningElement,api } from 'lwc';
 
-export default class FileUploader extends NavigationMixin(LightningElement) {
+export default class FileUploader extends LightningElement {
 
-    @api
-    recordId
      myRecordId='0015h00001ezA2SAAU';
-     allFilesWireData
-     allFiles
-
-
-     @wire(getUploadedFiles,{recoardId : recordId})
-     getAllFiles(value){
-        this.allFilesWireData = value
-        if (value.data) {
-            this.allFiles = value.data
-        }else if(value.err){
-            console.log('error',value.err);
-        }
-     }
 
     get acceptedFormats() {
         return ['.pdf', '.png'];
@@ -28,18 +11,4 @@ export default class FileUploader extends NavigationMixin(LightningElement) {
         const file= ev.detail.files;
         console.log('Files >7>',file.length);
     }
-
-
-    // file preview
-    navigateToFiles(ContentDocumentId) {
-        this[NavigationMixin.Navigate]({
-          type: 'standard__namedPage',
-          attributes: {
-              pageName: 'filePreview'
-          },
-          state : {
-              selectedRecordId:ContentDocumentId
-          }
-        })
-      }
 }
