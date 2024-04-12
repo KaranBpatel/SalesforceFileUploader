f477291a7d63be4ea169b34774898a4ad68c7fb7
Ben10 <bhashkar.nishad@eruditeworks.com>
2024-04-12 13:00:50 +0530

    impleted wire to getUploadedFiles

--- force-app/main/default/lwc/fileUploader/fileUploader.js
+++ force-app/main/default/lwc/fileUploader/fileUploader.js
@@ -1,9 +1,25 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import getUploadedFiles from '@salesforce/apex/FileController.getFiles';
 
 export default class FileUploader extends NavigationMixin(LightningElement) {
 
+    @api
+    recordId
      myRecordId='0015h00001ezA2SAAU';
+     allFilesWireData
+     allFiles
+
+
+     @wire(getUploadedFiles,{recoardId : recordId})
+     getAllFiles(value){
+        this.allFilesWireData = value
+        if (value.data) {
+            this.allFiles = value.data
+        }else if(value.err){
+            console.log('error',value.err);
+        }
+     }
 
     get acceptedFormats() {
         return ['.pdf', '.png'];
